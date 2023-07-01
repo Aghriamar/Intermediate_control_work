fabd704723509b14c0cac44840d8618a15d98390
Aghriamar <A1exXx88@inbox.ru>
2023-07-01 14:45:33 +0300

    Added documentation to the file ToyShop.java

--- src/toyshop/ToyShop.java
+++ src/toyshop/ToyShop.java
@@ -2,24 +2,48 @@ package toyshop;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Класс, представляющий магазин игрушек.
+ * Реализует функционал добавления новых игрушек, изменения веса (частоты выпадения) игрушек
+ * и организацию розыгрыша игрушек.
+ */
 public class ToyShop {
     private final List<Toy> toys;
 
+    /**
+     * Список призовых игрушек, ожидающих выдачи.
+     */
     private final List<Toy> prizeToys;
     private int totalToys;
 
+    /**
+     * Создает новый экземпляр класса ToyShop.
+     */
     public ToyShop() {
         toys = new ArrayList<>();
         prizeToys = new ArrayList<>();
         totalToys = 0;
     }
 
+    /**
+     * Добавляет новую игрушку в магазин.
+     *
+     * @param id идентификатор игрушки
+     * @param name название игрушки
+     * @param weight вес (частота выпадения) игрушки в процентах от 100
+     */
     public void addToy(int id, String name, int weight) {
         Toy toy = new Toy(id, name, weight);
         toys.add(toy);
         totalToys++;
     }
 
+    /**
+     * Обновляет вес (частоту выпадения) игрушки по её идентификатору.
+     *
+     * @param id идентификатор игрушки
+     * @param weight новый вес игрушки в процентах от 100
+     */
     public void updateWeight(int id, int weight) {
         for (Toy toy : toys) {
             if (toy.getId() == id) {
@@ -29,6 +53,10 @@ public class ToyShop {
         }
     }
 
+    /**
+     * Запускает розыгрыш игрушек.
+     * Выбирает призовые игрушки, записывает их в файл и уменьшает количество игрушек.
+     */
     public void startRaffle() {
         if (prizeToys.isEmpty()) {
             generatePrizeToys();
@@ -44,6 +72,9 @@ public class ToyShop {
         }
     }
 
+    /**
+     * Генерирует список призовых игрушек на основе их веса (частоты выпадения).
+     */
     private void generatePrizeToys() {
         Random random = new Random();
         for (Toy toy : toys) {
@@ -55,6 +86,12 @@ public class ToyShop {
         }
     }
 
+    /**
+     * Получает призовую игрушку из списка ожидания.
+     * Удаляет её из списка и возвращает.
+     *
+     * @return призовая игрушка
+     */
     private Toy getPrizeToy() {
         if (prizeToys.isEmpty()) {
             return null;
@@ -65,6 +102,11 @@ public class ToyShop {
         return prizeToy;
     }
 
+    /**
+     * Записывает выигрышную игрушку в файл.
+     *
+     * @param toy выигрышная игрушка
+     */
     private void writeToFile(Toy toy) {
         try {
             FileWriter writer = new FileWriter("prize_toys.txt", true);
